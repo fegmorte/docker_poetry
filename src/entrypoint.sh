d8caf611cc7dababad09c72a5920ab6af2e43e02
fegmorte <frederic.egmorte@tessi.fr>
2021-01-10 11:52:54 +0100

    Initial commit

---
+++ src/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for Postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "Postgres started"
+fi
+
+exec "$@"
